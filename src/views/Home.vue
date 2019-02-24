75370b1c7e2b24e12bac791a17fc0820c249d8fb
Michael Sanders <mike.gamedev@gmail.com>
2019-02-24 12:18:39 -0800

    Finished frontpage layout.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,14 @@
       </v-flex>
       <v-flex lg7 md7 sm7 xs7>
         <h2 align-center>Oh hey, I didn't see you there.</h2>
-        <h3>Thanks for coming to my site. If you came to checkout my writing, you should checkout my <a>Medium</a> profile. Want to see some some cool stuff I've made? Check my <a>Portfolio</a> out here. Want to see the who/what/when of my work history? Well we have a classic <a>resume</a> this way. Want to just say hi? Well then here's my </h3>
+        <h3>Thanks for coming to my site. If you came to checkout my writing, you should checkout my <a>Medium</a> profile. 
+        Want to see some some cool stuff I've made? Check my <a>Portfolio</a> out here. 
+        Want to see the who/what/when of my work history? Well we have a classic <a>resume</a> this way. 
+        Want to just say hi? Well then send an <a href="#"
+            data-contact="bWljaGFlbEBwaXhlbC1jdXIuaW8="
+            data-subj="UGl4ZWwtQ3VyLmlvIEVucXVpcnk="
+            onfocus="this.href = 'mailto:' + atob(this.dataset.contact) + '?subject=' + atob(this.dataset.subj || '')">email.</a>
+        </h3>
       </v-flex>
     </v-layout>
   </v-container>
