df0ab448283f4c21ab0c28535a888cfa54fb7aa0
Michael Sanders <mike.gamedev@gmail.com>
2019-03-10 14:00:44 -0700

    Changed resume url to https.

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid fill-height>
       <iframe
-        src="http://docs.google.com/gview?url=https://pixel-cur.io/doc/MichaelSandersResume.pdf&embedded=true"
+        src="https://docs.google.com/gview?url=https://pixel-cur.io/doc/MichaelSandersResume.pdf&embedded=true"
         style="width:100%; height:85vh;"
         frameborder="0"
       ></iframe>
